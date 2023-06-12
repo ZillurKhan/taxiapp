f6c617bbbdf213cfe99532018c84b61a402ef2cd
ZILLUR KHAN <46049488+ZillurKhan@users.noreply.github.com>
2023-06-12 12:20:33 +0600

    button css

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,3 @@
-/*================
- Template Name: AppCo App Landing Page Template
- Description: AppCo is app and product landing page template.
- Version: 1.0
- Author: https://themeforest.net/user/themetags
-=======================*/
-
-// TABLE OF CONTENTS
-// 1. fixed navbar
-// 2. page scrolling feature - requires jQuery Easing plugin
-// 3. closes the responsive menu on menu item click
-// 4. magnify popup video
-// 5. client testimonial slider
-// 6. Screenshots slider
-// 7. custom counter js with scrolling
-// 8. client-testimonial one item carousel
-// 9. our clients logo carousel
-// 10. our clients logo carousel
-// 11. wow js
-
 
 jQuery(function ($) {
 
@@ -61,7 +41,7 @@ jQuery(function ($) {
 
     // 5. client testimonial slider
     $('.client-testimonial').owlCarousel({
-        loop:false,
+        loop:true,
         margin:30,
         items:1,
         nav: true,
